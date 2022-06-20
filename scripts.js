4344ed410542c6c2d76b4f6799861807e2f9638b
drichman <drichman813@gmail.com>
2022-06-19 20:22:24 -0600

    Added customizable grid size

--- scripts.js
+++ scripts.js
@@ -1,22 +1,49 @@
-function createGrid() {
+function createGrid(size) {
     const container = document.querySelector('.grid-container');
-    createColumn(container);
+    createColumn(container, size);
     const columns = document.querySelectorAll('.grid-column');
-    columns.forEach(column => createRow(column));
+    columns.forEach(column => createRow(column, size));
 }
 
-function createColumn(container) {
-    for (i = 0; i < 16; i++) {
+function createColumn(container, size) {
+    for (i = 0; i < size; i++) {
         const column = document.createElement('div');
         column.classList.add('grid-column');
         container.append(column);
     }
 }
 
-function createRow(column) {
-    for (i = 0; i < 16; i++) {
+function createRow(column, size) {
+    for (i = 0; i < size; i++) {
         const tile = document.createElement('div');
         tile.classList.add('grid-tile');
         column.append(tile);
     }
+    const tiles = document.querySelectorAll('.grid-tile');
+    tiles.forEach(tile => tile.addEventListener('mouseover', colorTile));
 }
+
+function colorTile(e) {
+    e.target.classList.add('colored');
+    e.stopPropagation();
+}
+
+function openCustomizeGridPopup() {
+    console.log("Test");
+    const size = window.prompt("Enter the size of grid you want (up to 100).", 16);
+    resetGrid();
+    createGrid(size);
+}
+
+function resetGrid() {
+    const columns = document.querySelectorAll('.grid-column');
+    columns.forEach(column => column.remove());
+}
+
+function initialize() {
+    createGrid(16);
+    const customizebutton = document.querySelector('#customize-grid-button');
+    customizebutton.addEventListener('click', () => openCustomizeGridPopup());
+}
+
+initialize();
